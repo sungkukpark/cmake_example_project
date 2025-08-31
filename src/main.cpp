c72af6280555c9c3948303b11074088867b0b29f
Sungkuk Park <sungkuk.park@wooga.net>
2025-08-31 18:11:08 +0200

    add initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2025 Sungkuk Park, ALl Rights Reserved.
+// AUthor: Sungkuk Park
+
+#include <cstdint>
+#include <iostream>
+
+int32_t main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
